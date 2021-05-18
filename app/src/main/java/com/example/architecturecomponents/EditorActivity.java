1fb55f67c459ee5d90ae6999403fd1ccff63717a
Julia <2912>
2021-05-18 19:47:02 +0300

    Create the note editor activity

---
+++ app/src/main/java/com/example/architecturecomponents/EditorActivity.java
@@ -0,0 +1,26 @@
+package com.example.architecturecomponents;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+
+    }
+}
--- app/src/main/java/com/example/architecturecomponents/MainActivity.java
+++ app/src/main/java/com/example/architecturecomponents/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.architecturecomponents;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.architecturecomponents.model.NoteEntity;
@@ -24,12 +25,20 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "LOG_TAG";
+
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+
+    @OnClick(R.id.fab)
+    void fabClickHandler() {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivity(intent);
+    }
 
     private List<NoteEntity> notesData = new ArrayList<>();
     private NotesAdapter mAdapter;
@@ -44,14 +53,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initRecyclerView();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         notesData.addAll(SampleData.getNotes());
 
