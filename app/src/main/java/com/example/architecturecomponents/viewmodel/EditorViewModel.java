b66531ddf2d9b0c7f50a4b4142fdf92cf67185db
Julia <2912>
2021-05-20 13:13:54 +0300

    Create a new note

--- app/src/main/java/com/example/architecturecomponents/viewmodel/EditorViewModel.java
+++ app/src/main/java/com/example/architecturecomponents/viewmodel/EditorViewModel.java
@@ -9,8 +9,11 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.architecturecomponents.database.AppRepository;
 import com.example.architecturecomponents.database.NoteEntity;
 
+import java.util.Date;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import static android.text.TextUtils.isEmpty;
 
 public class EditorViewModel extends AndroidViewModel {
 
@@ -37,9 +40,12 @@ public class EditorViewModel extends AndroidViewModel {
     public void saveNote(String noteText) {
         NoteEntity note = mLiveNote.getValue();
         if (note == null) {
-
+            if (isEmpty(noteText.trim())) {
+                return;
+            }
+            note = new NoteEntity(new Date(), noteText.trim());
         } else {
-            note.setText(noteText);
+            note.setText(noteText.trim());
         }
 
         mRepository.insertNote(note);
